ac7e8da307f3b651f0b5d2e22b654fc02413c30e
Naseschwarz <naseschwarz@0x53a.de>
2025-03-05 18:26:33 +0100

    Move Wayland/X string copying out of copy_string
    Copying logic seems too nested to comprehend with the introcution of two
    paths towards OSC52 otherwise.

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,5 +1,4 @@
 use anyhow::{anyhow, Result};
-use base64::prelude::{Engine, BASE64_STANDARD};
 use std::io::Write;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
@@ -68,7 +67,9 @@ fn is_wsl() -> bool {
 // This enables copying even if there is no Wayland or X socket available,
 // e.g. via SSH, as long as it supported by the terminal.
 // See https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h3-Operating-System-Commands
+#[cfg(all(target_family = "unix", not(target_os = "macos")))]
 fn copy_string_osc52(text: &str, out: &mut impl Write) -> Result<()> {
+	use base64::prelude::{Engine, BASE64_STANDARD};
 	const OSC52_DESTINATION_CLIPBOARD: char = 'c';
 	write!(
 		out,
@@ -80,33 +81,47 @@ fn copy_string_osc52(text: &str, out: &mut impl Write) -> Result<()> {
 }
 
 #[cfg(all(target_family = "unix", not(target_os = "macos")))]
-pub fn copy_string(text: &str) -> Result<()> {
-	if std::env::var("WAYLAND_DISPLAY").is_ok() {
-		if exec_copy_with_args("wl-copy", &[], text, false).is_err() {
-			copy_string_osc52(text, &mut std::io::stdout())?;
-		}
+fn copy_string_wayland(text: &str) -> Result<()> {
+	if exec_copy_with_args("wl-copy", &[], text, false).is_ok() {
+		return Ok(());
 	}
 
-	if is_wsl() {
-		return exec_copy_with_args("clip.exe", &[], text, false);
-	}
+	copy_string_osc52(text, &mut std::io::stdout())
+}
 
+#[cfg(all(target_family = "unix", not(target_os = "macos")))]
+fn copy_string_x(text: &str) -> Result<()> {
 	if exec_copy_with_args(
 		"xclip",
 		&["-selection", "clipboard"],
 		text,
 		false,
 	)
-	.is_err()
+	.is_ok()
 	{
-		if exec_copy_with_args("xsel", &["--clipboard"], text, true)
-			.is_err()
-		{
-			copy_string_osc52(text, &mut std::io::stdout())?;
-		}
+		return Ok(());
 	}
 
-	Ok(())
+	if exec_copy_with_args("xsel", &["--clipboard"], text, true)
+		.is_ok()
+	{
+		return Ok(());
+	}
+
+	copy_string_osc52(text, &mut std::io::stdout())
+}
+
+#[cfg(all(target_family = "unix", not(target_os = "macos")))]
+pub fn copy_string(text: &str) -> Result<()> {
+	if std::env::var("WAYLAND_DISPLAY").is_ok() {
+		return copy_string_wayland(text);
+	}
+
+	if is_wsl() {
+		return exec_copy_with_args("clip.exe", &[], text, false);
+	}
+
+	copy_string_x(text)
 }
 
 #[cfg(any(target_os = "macos", windows))]
