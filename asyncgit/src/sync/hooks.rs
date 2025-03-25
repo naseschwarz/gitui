d25efdc2d25d08beb86b4e820362375a560da231
Joshix <joshix.dev@mailbox.org>
2025-03-25 20:00:00 +0000

    Revert "more trimming"
    This reverts commit a4e2f54ec9540b7aa717fcb90410a8144f224220.

--- asyncgit/src/sync/hooks.rs
+++ asyncgit/src/sync/hooks.rs
@@ -24,15 +24,7 @@ impl From<git2_hooks::HookResult> for HookResult {
 			} => {
 				const ANSI_CLEAR: &str = "\x1B[H\x1B[2J\x1B[3J";
 
-				let mut combined = stdout;
-
-				if let Some(trimmed) =
-					combined.strip_suffix(ANSI_CLEAR)
-				{
-					combined.truncate(trimmed.len());
-				}
-
-				combined.push_str(&stderr);
+				let mut combined = format!("{stdout}{stderr}");
 
 				if let Some(trimmed) =
 					combined.strip_suffix(ANSI_CLEAR)
