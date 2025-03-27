90a0f68d5002b5debb9fb30e6864e27f777e05b1
Naseschwarz <naseschwarz@0x53a.de>
2025-03-27 22:06:43 +0100

    rebase fix

--- asyncgit/src/file_history.rs
+++ asyncgit/src/file_history.rs
@@ -8,7 +8,7 @@ use crate::{
 		commit_files::{
 			commit_contains_file, commit_detect_file_rename,
 		},
-		CommitId, CommitInfo, RepoPath,
+		CommitId, CommitInfo, LogWalker, RepoPath,
 		SharedCommitFilterFn,
 	},
 	AsyncGitNotification,
